f8f490423d3b54174f519f91b463039065a31630
Ken Dale <ken@kendaleiv.com>
2016-05-06 20:11:21 -0400

    Create RimDev.Stuntman demo

---
+++ src/StuntmanDemo.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace StuntmanDemo.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Secure()
+        {
+            if (User.Identity.IsAuthenticated == true)
+            {
+                return View();
+            }
+            else
+            {
+                return new HttpUnauthorizedResult();
+            }
+        }
+    }
+}
---
+++ src/StuntmanDemo.Mvc/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Owin;
+using Owin;
+using RimDev.Stuntman.Core;
+
+[assembly: OwinStartup(typeof(StuntmanDemo.Mvc.Startup))]
+
+namespace StuntmanDemo.Mvc
+{
+    public class Startup
+    {
+        public static readonly StuntmanOptions StuntmanOptions = new StuntmanOptions();
+
+        public void Configuration(IAppBuilder app)
+        {
+            StuntmanOptions
+                .AddUser(new StuntmanUser("anonymous-1", "Anonymous"))
+                .AddUser(new StuntmanUser("employee-1", "Employee 1")
+                    .AddClaim(System.Security.Claims.ClaimTypes.Role, "Employee"))
+                .AddUser(new StuntmanUser("administrator-1", "Administrator 1")
+                    .AddClaim(System.Security.Claims.ClaimTypes.Role, "Employee")
+                    .AddClaim(System.Security.Claims.ClaimTypes.Role, "Administrator"));
+
+            if (System.Web.HttpContext.Current.IsDebuggingEnabled)
+            {
+                app.UseStuntman(StuntmanOptions);
+            }
+        }
+    }
+}
