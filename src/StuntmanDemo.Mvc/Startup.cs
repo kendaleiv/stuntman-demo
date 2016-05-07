811ba14a7c66a83ee859b0c107cd2bdeead367f4
Ken Dale <ken@kendaleiv.com>
2016-05-07 12:51:43 -0400

    Add blank lines for readability

--- src/StuntmanDemo.Mvc/Startup.cs
+++ src/StuntmanDemo.Mvc/Startup.cs
@@ -14,8 +14,10 @@ namespace StuntmanDemo.Mvc
         {
             StuntmanOptions
                 .AddUser(new StuntmanUser("anonymous-1", "Anonymous"))
+
                 .AddUser(new StuntmanUser("employee-1", "Employee 1")
                     .AddClaim(System.Security.Claims.ClaimTypes.Role, "Employee"))
+
                 .AddUser(new StuntmanUser("administrator-1", "Administrator 1")
                     .AddClaim(System.Security.Claims.ClaimTypes.Role, "Employee")
                     .AddClaim(System.Security.Claims.ClaimTypes.Role, "Administrator"));
